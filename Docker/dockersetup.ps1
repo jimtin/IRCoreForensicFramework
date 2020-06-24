378e9abd192b579d52f320bf57a54304200a6db6
James Hinton <jameshin84@gmail.com>
2020-06-25 07:45:29 +1000

    Created a docker and docker setup file

---
+++ Docker/dockersetup.ps1
@@ -0,0 +1,41 @@
+#Requires -Version 7
+#Requires -RunAsAdministrator
+
+# Get a list of modules from the modules file 
+$modules = Get-Content -Path .\modulemanifest.txt
+
+foreach ($cmdlet in $modules){
+    $messagestring = "Importing cmdlet: " + $cmdlet
+    Write-Information -InformationAction Continue -MessageData $messagestring
+    # Import the module
+    Import-Module -Name $cmdlet -Force
+}
+
+# Ensure executables which will help are downloaded 
+Write-HostHunterInformation -MessageData "Checking core executeables are downloaded"
+Get-SetupExecuteables
+
+# Set up the Python analysis folder
+$analysis = Set-PythonAnalysisList
+while($analysis -ne $true){
+    Start-Sleep -Seconds 1
+    $analysis = Set-PythonAnalysisList
+}
+
+# Copy Volatility into python analysis folder
+$volatility = Copy-Volatility
+
+# Import Volatility3 Symbols tables for all operating systems
+Write-HostHunterInformation -MessageData "Ensuring Volatility3 Symbols Tables are available"
+Import-VolatilitySymbols
+
+# Make sure Prefetch Parser is ready
+Expand-PrefetchParser
+
+# Set up the target tracking variable
+if((Get-Variable -Name GlobalTargetList -ErrorAction SilentlyContinue) -eq $null){
+    New-Variable -Name "GlobalTargetList" -Scope global -Visibility Public -Value @{}
+} 
+
+Write-HostHunterInformation -MessageData "GlobalTargetList variable set" -ForegroundColor "Blue"
+
